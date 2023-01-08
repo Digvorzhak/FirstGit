97bf5831e6fcdd079fa26ee8d1dd5de6016403f6
Digvorzhak <shon.h777@gmail.com>
2023-01-08 11:24:50 +0200

    task 2.2

---
+++ JavaScript/2.2_functions.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const tellFortune = (jobTitle, location, partnerName, numberOfChildren) => `You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children`;
+
+console.log(tellFortune("Secret Agent", "Moscow", "Nitzan", 0));
