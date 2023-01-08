79a81f29e37a0725e8f86457c9c0948f94116fde
Digvorzhak <shon.h777@gmail.com>
2023-01-08 13:57:14 +0200

    refactor 2.4

--- JavaScript/2.4_date.js
+++ JavaScript/2.4_date.js
@@ -9,7 +9,11 @@ console.log(today.getFullYear());
 
 console.log(today);
 
+// Attempt #1
+
 const getDate = (date) => `Today is ${date.getDay()} the ${date.getDate()} of ${date.getMonth()} ${date.getFullYear()}`;
+
+// Attempt #2
 
 const getDate1 = function (date) {
   const date1 = date.toDateString();
@@ -18,3 +22,17 @@ const getDate1 = function (date) {
 };
 
 console.log(getDate1(new Date()));
+
+// Attempt #3
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const getDate2 = function (date) {
+  let day = days[date.getDay()];
+  let month = months[date.getMonth()];
+
+  return `Today is ${day} the ${date.getDate()} of ${month} ${date.getFullYear()}`;
+};
+
+console.log(getDate2(new Date()));
