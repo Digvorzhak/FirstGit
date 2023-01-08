f570c1f410cd3d7ee52de1545d6f1d0578a179b6
Digvorzhak <shon.h777@gmail.com>
2023-01-08 10:44:16 +0200

    task

---
+++ JavaScript/1.1_primitive_data_types.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const string = "hi";
+const num = 42;
+const account = {
+  password: 1234,
+  name: "Sean",
+  age: 24,
+  email: "shon.h777@gmail.com",
+};
+const boolean = false;
+
+const nu = null;
+
+const mySymbol = Symbol("Some Test");
+
+let undefined1;
+
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof account);
+console.log(typeof boolean);
+console.log(typeof undefined1);
+console.log(typeof nu);
+console.log(typeof mySymbol);
