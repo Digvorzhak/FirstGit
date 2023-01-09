ea5db1413bb5ea491b60a36846b2e3dd0354f822
Digvorzhak <shon.h777@gmail.com>
2023-01-09 10:58:09 +0200

    task

---
+++ JavaScript/3.3_functions.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Task 1
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const poland = percentageOfWorld1(38);
+console.log(poland);
+
+const spain = percentageOfWorld1(47);
+console.log(spain);
+
+const sweden = percentageOfWorld1(10);
+console.log(sweden);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+percentageOfWorld2(5); // Norway
+percentageOfWorld2(212); // Brazil
+percentageOfWorld2(109); // Philippines
