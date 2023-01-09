f93e1a2cc5cbefdd4aeb18c9f645c030423f9b74
Digvorzhak <shon.h777@gmail.com>
2023-01-09 11:47:48 +0200

    task

--- JavaScript/4.2_grade_assigner.js
+++ JavaScript/4.2_grade_assigner.js
@@ -1,5 +1,5 @@
 "use strict";
 
 const gradeAssigner = (grade) => {
-  return grade < 65 ? "F" : grade <= 69 ? "D" : grade <= 79 ? "C" : grade <= 89 ? "B" : "A";
+  return grade <= 64 ? "F" : grade <= 69 ? "D" : grade <= 79 ? "C" : grade <= 89 ? "B" : "A";
 };
