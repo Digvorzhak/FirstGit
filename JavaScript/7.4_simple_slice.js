7eb230f141908a415c5bb22a2790f8f7e485ed0b
Digvorzhak <shon.h777@gmail.com>
2023-01-10 18:31:39 +0200

    task

--- JavaScript/7.4_simple_slice.js
+++ JavaScript/7.4_simple_slice.js
@@ -1,7 +1,6 @@
 "use strict";
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-fruits.splice(2, 0, "Lemon");
-fruits.splice(3, 0, "Kiwi");
+fruits.splice(2, 0, "Lemon", "Kiwi");
 
 console.log(fruits);
