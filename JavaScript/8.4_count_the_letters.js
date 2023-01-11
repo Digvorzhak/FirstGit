1e16b485f41be69b91b9e63633ebcdfc4b047375
Digvorzhak <shon.h777@gmail.com>
2023-01-11 18:19:30 +0200

    task

---
+++ JavaScript/8.4_count_the_letters.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const lettersCounter = (array) => {
+  const lettersHive = {};
+  let fullResult = [];
+
+  let letters = array.join("").toLowerCase();
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] !== " ") lettersHive[letters[i]] = 0;
+  }
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] in lettersHive) lettersHive[letters[i]]++;
+  }
+  return (fullResult = [lettersHive, Math.max(...Object.values(lettersHive))]);
+};
+
+const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+
+console.log(lettersCounter(array));
