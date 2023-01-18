84a3a907403aed4438557d8480b072b9bd282231
Digvorzhak <shon.h777@gmail.com>
2023-01-18 18:10:29 +0200

    task

---
+++ JavaScript/15.3_sum_of_even.js
@@ -0,0 +1,9 @@
+function getSumOfEven(arr) {
+  return arr[2] + arr[4] + arr[6] + arr[8] + arr[10];
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+function getSumOfEven(arr) {
+  return arr[2] + arr[4] + arr[6] + arr[8] + arr[9];
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
