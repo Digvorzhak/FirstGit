3f13e9486da4c73cf60a78bc6134cddfdf64b673
Digvorzhak <shon.h777@gmail.com>
2023-01-18 18:29:37 +0200

    tas

---
+++ JavaScript/15.5_occurences.js
@@ -0,0 +1,21 @@
+function countOccurrences(str, char) {
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === char) {
+      counter + 1; // ????????????? there is no assignment to counter.
+    }
+  }
+  return counter;
+}
+console.log(countOccurrences("ini mini miny moe", "n"));
+
+function countOccurrences(str, char) {
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === char) {
+      counter++;
+    }
+  }
+  return counter;
+}
+countOccurrences("ini mini miny moe", "n");
