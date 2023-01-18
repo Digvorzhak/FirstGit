267ded2f94828ad199ab0e79d536ccba896a7e28
Digvorzhak <shon.h777@gmail.com>
2023-01-18 17:59:07 +0200

    task

--- JavaScript/15.1_sum.js
+++ JavaScript/15.1_sum.js
@@ -11,14 +11,14 @@ function getSum(arr1, arr2) {
 }
 getSum([1, 2, 3][(5, 66, 23)]); // NO COMMA BETWEEN ARGUMENTS
 
-function getSumFixed(arr1, arr2) {
-  let sum = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    sum += arr1[i];
-  }
-  for (let i = 0; i < arr2.length; i++) {
-    sum += arr2[i];
-  }
-  return sum;
-}
-console.log(getSumFixed([1, 2, 3], [5, 66, 23]));
+// function getSumFixed(arr1, arr2) {
+//   let sum = 0;
+//   for (let i = 0; i < arr1.length; i++) {
+//     sum += arr1[i];
+//   }
+//   for (let i = 0; i < arr2.length; i++) {
+//     sum += arr2[i];
+//   }
+//   return sum;
+// }
+// console.log(getSumFixed([1, 2, 3], [5, 66, 23]));
---
+++ JavaScript/15.2.find_smallest.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function findSmallest(a, b, c) {
+  if (a > b > c) {
+    return c;
+  } else if (a > c > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+console.log(findSmallest(52, 66, 2));
+
+function findSmallest(a, b, c) {
+  if (a < b && a < c) {
+    return a;
+  } else if (b < a && b < c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
+console.log(findSmallest(52, 66, 2));
