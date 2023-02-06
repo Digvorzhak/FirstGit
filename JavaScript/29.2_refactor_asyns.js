b24334ea31c5af9cb6c1e390d6af658f49ef68c5
Digvorzhak <shon.h777@gmail.com>
2023-02-06 22:00:23 +0200

    task

---
+++ JavaScript/29.2_refactor_asyns.js
@@ -0,0 +1,18 @@
+const jokeTitle = document.querySelector(".joke-title");
+const joke = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+// const countriesContainer = document.querySelector(".countriesContainer");
+
+const jokeToday = async () => {
+  const res = await fetch("https://api.jokes.one/jod");
+  const data = await res.json();
+  console.log(data);
+  const title = data.contents.jokes[0].joke.title;
+  console.log(title);
+  const jokeText = data.contents.jokes[0].joke.text;
+  console.log(jokeText);
+  jokeTitle.innerHTML = `${title}`;
+  joke.innerHTML = `${jokeText}`;
+};
+
+btn.addEventListener("click", jokeToday);
